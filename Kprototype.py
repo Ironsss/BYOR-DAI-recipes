5fba5c2a94329a738cf457eca48e5f93c1e77fdf
David Alexis G <davidalexis@comunidad.unam.mx>
2024-03-22 13:41:19 -0500

    Update Kprototype.py

--- Kprototype.py
+++ Kprototype.py
@@ -22,7 +22,7 @@ class KPrototypesTransformer(CustomUnsupervisedTransformer):
         self.init = init
 
     def fit_transform(self, X: dt.Frame, y: np.array = None):
-        _modules_needed_by_name = ["kmodes==0.12.2"]
+        _modules_needed_by_name = ["kmodes"]
 
         # KPrototypes needs to know which columns are categorical. Assuming last column is categorical here as an example.
         categorical_indices = [X.shape[1] - 1]  # Example: Assuming last column is categorical
